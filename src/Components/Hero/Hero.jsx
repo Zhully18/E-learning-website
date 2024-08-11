844d551e85fb235a1e16e815decab2c46224f74e
Zhully <agunregezhuliakho@gmail.com>
2024-08-10 17:02:25 -0700

    starter

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,46 +1,8 @@
 import React from 'react'
-import './Hero.css'
-import { GiRapidshareArrow } from 'react-icons/gi'
-import student from '../../assets/s2.jpg'
-import student1 from '../../assets/s1.jpg'
-import student2 from '../../assets/s3.jpg'
-import student3 from '../../assets/s4.jpg'
-import { easeInOut, motion } from 'framer-motion'
 
 const Hero = () => {
   return (
-    <div>
-      <div className="hero">
-        <div className="hero_content">
-          {/* lets animate the h1 content so it will slide from
-          the left axis to the right axis */}
-          <motion.h1
-          initial= {{opacity: 0, x: -100}}
-          animate= {{opacity: 1, x:0 }}
-          transition={{duration: 0.4, delay: 0.2, ease: easeInOut}}
-          exit={{
-            opacity: 0, x: -100, transition: { duration: 0.2}
-          }}
-          ><span>Empower </span> Your Future With Cutting-Edge <span>Skills</span></motion.h1>
-          <p>Unlock your potential with courses designed to help you thrive in the digital age. From coding to creative arts, we provide the tools you need to succeed. </p>
-          <div className="buttons">
-            <a href="#" className="cta-button">Enroll Now</a>
-            <a href="#" className="courses-button">Explore Courses</a>
-          </div>
-          <GiRapidshareArrow className='hero_icon'/>
-        </div>
-        <div className="hero_images">
-          <div className="img_top">
-            <img src={student} alt="" className='stuent_img'/>
-            <img src={student1} alt="" className='stuent_img_one'/>
-          </div>
-          <div className="img_bottom">
-            <img src={student2} alt="" className='stuent_img_two'/>
-            <img src={student3} alt="" className='stuent_img_three'/>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div>Hero</div>
   )
 }
 
--- src/Components/Instructor/Instructor.jsx
+++ src/Components/Instructor/Instructor.jsx
@@ -1,38 +1,8 @@
 import React from 'react'
-import man from '../../assets/man3.jpg'
-import './Instructor.css'
-import { easeInOut, motion } from 'framer-motion'
-
-const slideRight = (delay) => {
-  return {
-    hidden: { opacity: 0 , x: -100},
-    show: {opacity: 1, x:0, transition: {duration: 0.5, delay: delay, ease: easeInOut}}
-  }
-}
 
 const Instructor = () => {
   return (
-    <div>
-        <div className="instructor">
-            <div className="left">
-                <motion.img
-                variants={slideRight(0.2)}
-                initial="hidden"
-                whileInView= "show"
-                 src={man} alt="" />
-            </div>
-            <motion.div
-            variants={slideRight(0.7)}
-            initial="hidden"
-            whileInView= "show"
-             className="right">
-                <h2>Become An Instructor</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus expedita beatae necessitatibus magnam neque omnis, ad corporis voluptatum rem obcaecati veniam delectus numquam consectetur. Qui assumenda iusto vitae labore numquam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis laboriosam modi, fugiat veritatis est odit illum porro, adipisci repudiandae recusandae aut tempora vel repellendus et accusamus natus possimus nisi deserunt?</p>
-                <button>Register Today</button>
-            </motion.div>
-        </div>
-    </div>
+    <div>Instructor</div>
   )
 }
 
